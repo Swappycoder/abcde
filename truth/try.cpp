d6a94ebf78b21b775a9172646f471989d22c142a
Mohit Khade <mohitkhade11@gmail.com>
2023-02-14 21:02:34 +0530

    test1

---
+++ truth/try.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()    
+{
+    cout << "Nitish " << endl;
+    cout << "Mohit is added" << endl;
+    cout<<"WELCOME TO MY YOUTUBE CHANNEL GAIS"<<endl;
+    cout<<"Aarul is gay"<<endl;
+    return 0;
+}
